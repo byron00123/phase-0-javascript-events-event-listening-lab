06e4e5305f327546b5e3d0ed1ecb2dda9e4f06ae
Byron Omondi <omondibyron25@gmail.com>
2023-03-25 09:50:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
-}
+    var input = document.getElementById('button');
+    input.addEventListener('click', function() {
+      alert('Input clicked!');
+    });
+  }
+  
+  // Call the function to bind the event listener
+  addingEventListener();
+  
+
+
